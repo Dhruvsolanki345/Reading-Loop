45735a818b3e96befa4ec56d078817eb711ef1ab
Rajas-B <bondalerajas@gmail.com>
2022-11-09 00:38:04 +0530

    Login, Registration test cases complete. CSV files to log the tests

--- testing/registration_testing.py
+++ testing/registration_testing.py
@@ -3,6 +3,8 @@ import time
 from selenium.webdriver.common.keys import Keys  
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
+import pandas as pd
+
 # driver = webdriver.Chrome("C:/Users/Rajas/chromedriver_win32/chromedriver.exe")
 ser = Service("C:/Users/Rajas/geckodriver_win32/geckodriver.exe")
 # driver = webdriver.Firefox(executable_path= "C:/Users/Rajas/geckodriver_win32/geckodriver.exe")
@@ -22,7 +24,19 @@ time.sleep(2)
 myele = driver.find_element("xpath", "//span[contains(.,'Signup Now')]")
 myele.click()
 time.sleep(1)
-signups = [{"name": "Rajas", "email": "rajas@gmail.com", "phone": "9969342104", "password": "password", "agreeTerms": True, "result": ""}]
+signups = [{"name": "",       "email": "rajas@gmail.com", "phone": "9969342104",   "password": "password", "agreeTerms": True, "result": ""},
+            {"name": "Rajas", "email": "rajas@gmail.com", "phone": "9969342104",   "password": "password", "agreeTerms": True, "result": ""},
+            {"name": "",       "email": ""              ,"phone": "",              "password": "",         "agreeTerms": True, "result": ""},
+            {"name": "Rajas", "email": "",                "phone": "9969342104",   "password": "password", "agreeTerms": True, "result": ""},
+            {"name": "Rajas", "email": "rajas",           "phone": "9969342104",   "password": "password", "agreeTerms": True, "result": ""},
+            {"name": "Rajas", "email": "rajas@gmail.com", "phone": "",             "password": "password", "agreeTerms": True, "result": ""},
+            {"name": "Rajas", "email": "rajas@gmail.com", "phone": "abcdefgh",     "password": "password", "agreeTerms": True, "result": ""},
+            {"name": "Rajas", "email": "rajas@gmail.com", "phone": "996934210",    "password": "password", "agreeTerms": True, "result": ""},
+            # {"name": "Rajas", "email": "rajas@gmail.com", "phone": "996934210412", "password": "password", "agreeTerms": True, "result": ""},
+            {"name": "Rajas", "email": "rajas@gmail.com", "phone": "9969342104",   "password": "",         "agreeTerms": True, "result": ""},
+            {"name": "Rajas", "email": "rajas@gmail.com", "phone": "9969342104",   "password": "pass",     "agreeTerms": True, "result": ""},
+            {"name": "Rajas", "email": "rajas@gmail.com", "phone": "9969342104",   "password": "password", "agreeTerms": False, "result": ""},
+            ]
 name = driver.find_element("name", "name")
 email = driver.find_element("name", "email")
 phone = driver.find_element("name", "phone")
@@ -31,7 +45,14 @@ agreeTerms = driver.find_element("name", "agreeTerms")
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 myele = driver.find_element("xpath", "//button[@class='rounded-filled-btn'][contains(., 'Sign Up')]")
 for i in signups:
-    time.sleep(1)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(2)
+    name = driver.find_element("name", "name")
+    email = driver.find_element("name", "email")
+    phone = driver.find_element("name", "phone")
+    password = driver.find_element("name", "password")
+    agreeTerms = driver.find_element("name", "agreeTerms")
+    myele = driver.find_element("xpath", "//button[@class='rounded-filled-btn'][contains(., 'Sign Up')]")
     name.clear()
     email.clear()
     phone.clear()
@@ -50,5 +71,7 @@ for i in signups:
     driver.find_element("css selector", ".SnackbarItem-action").find_element("tag name", "button").click()
     time.sleep(1)
 print(signups)
+df = pd.DataFrame.from_dict(signups)
+df.to_csv("registration.csv")
 driver.close()  
 print("sample test case successfully completed")  
