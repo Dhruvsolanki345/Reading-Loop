2ead3eda585e2dc0d1b4c1da19b9a412a4a06468
Rajas-B <54888083+Rajas-B@users.noreply.github.com>
2022-11-09 11:08:10 +0530

    Input from csv

--- testing/login_testing.py
+++ testing/login_testing.py
@@ -1,29 +1,25 @@
 from selenium import webdriver
 import time
-from selenium.webdriver.common.keys import Keys  
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 import pandas as pd
-# driver = webdriver.Chrome("C:/Users/Rajas/chromedriver_win32/chromedriver.exe")
+from csv import DictReader
+ 
 ser = Service("C:/Users/Rajas/geckodriver_win32/geckodriver.exe")
-# driver = webdriver.Firefox(executable_path= "C:/Users/Rajas/geckodriver_win32/geckodriver.exe")
 op = webdriver.FirefoxOptions()
-
+ 
 options = Options()
 options.binary_location = "C:/Program Files/Mozilla Firefox/firefox.exe"
-
+ 
 driver = webdriver.Firefox(service=ser, options=options)
 print("Login test cases started")  
 driver.get("https://reading-loop-528f8.web.app/login")
 driver.maximize_window()  
 time.sleep(2)
-signups = [{"email": "", "password": "Kk4GLcjLT5mUbdX" , "result": ""},
-            {"email": "rajas.bondale", "password": "Kk4GLcjLT5mUbdX" , "result": ""},
-            {"email": "12312212", "password": "Kk4GLcjLT5mUbdX" , "result": ""},
-            {"email": "rajas.bondale@somaiya.edu", "password": "" , "result": ""},
-            {"email": "", "password": "" , "result": ""},
-            {"email": "rajas.bondale@somaiya.edu", "password": "Kk4GLcjLT5mUbdX" , "result": ""},
-            ]
+with open("login_input.csv", 'r') as f:
+    dict_reader = DictReader(f)
+    signups = list(dict_reader)
+ 
 email = driver.find_element("name", "email")
 email.send_keys("email")
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
